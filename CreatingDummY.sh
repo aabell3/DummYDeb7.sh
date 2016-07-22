7d7b9de0fa888f44876405ab161bb4daad527b59
creatingdummy <Fadhil.morshidi2676@gmail.com>
2016-07-22 19:01:33 +0800

    Update CreatingDummY.sh

--- CreatingDummY.sh
+++ CreatingDummY.sh
@@ -2,7 +2,7 @@
 # initialisasi var
 
 export DEBIAN_FRONTEND=noninteractive
-OS=`uname -m`;
+#OS=`uname -m`;
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
